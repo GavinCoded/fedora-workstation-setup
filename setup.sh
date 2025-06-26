6f61be699d4d01256d10fa53b40622a4332be2b3
GavinStrikes <105064040+GavinCoded@users.noreply.github.com>
2025-06-26 19:05:56 -0400

    Code Cleanup & Add Prism Launcher

--- setup.sh
+++ setup.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
-# stop if anything fails
 set -e
 
-# basic packages
-echo "installing applications..."
-sudo dnf update -y
-sudo dnf install -y nodejs npm discord flatpak telegram-desktop steam lxrandr obs-studio wine winetricks bottles lutris htop fastfetch
+install_dnf() {
+    echo "installing $1..."
+    sudo dnf install -y "$1"
+}
 
-# flatpak stuff
+install_flatpak() {
+    echo "installing $1..."
+    flatpak install -y flathub "$1"
+}
+
+echo "updating system..."
+sudo dnf update -y
+
+echo "installing basic packages..."
+for pkg in nodejs npm discord flatpak telegram-desktop steam lxrandr obs-studio wine winetricks bottles lutris htop fastfetch; do
+    install_dnf "$pkg"
+done
+
 echo "setting up flatpak..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# flatpak applications 
-flatpak install -y flathub com.spotify.Client com.bitwarden.desktop com.jetbrains.IntelliJ-IDEA-Community com.jetbrains.WebStorm com.parsecgaming.parsec io.exodus.Exodus com.termius.Termius org.videolan.VLC
+echo "installing flatpak apps..."
+apps=(
+    "com.spotify.Client"
+    "com.bitwarden.desktop" 
+    "com.jetbrains.IntelliJ-IDEA-Community"
+    "com.jetbrains.WebStorm"
+    "com.parsecgaming.parsec"
+    "io.exodus.Exodus"
+    "com.termius.Termius"
+    "org.videolan.VLC"
+)
+for app in "${apps[@]}"; do
+    install_flatpak "$app"
+done
 
-# mullvad vpn
 echo "installing mullvad..."
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/rpm/mullvad-keyring.asc
 sudo wget -O /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
-sudo dnf install -y mullvad-vpn
+install_dnf mullvad-vpn
 
-# vscode (shit)
+# SHIT CODE!
 echo "installing vscode..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo tee /etc/yum.repos.d/vscode.repo >/dev/null <<< '[code]
@@ -29,18 +51,21 @@ baseurl=https://packages.microsoft.com/yumrepos/vscode
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc'
-sudo dnf install -y code
+install_dnf code
 
-# rpm fusion (needed for vbox and others)
 echo "adding rpm fusion..."
 sudo dnf install -y "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-# virtualbox + deps
-echo "installing virtualbox and deps..."
-sudo dnf install -y gcc make perl kernel-devel kernel-headers dkms elfutils-libelf-devel qt5-qtx11extras virtualbox
+echo "installing virtualbox..."
+for pkg in gcc make perl kernel-devel kernel-headers dkms elfutils-libelf-devel qt5-qtx11extras virtualbox; do
+    install_dnf "$pkg"
+done
 sudo akmods
 sudo systemctl restart vboxdrv
 sudo usermod -aG vboxusers "$USER"
 
-echo
-echo "all done. reboot or relog to finish setup."
+echo "adding prismlauncher..."
+sudo dnf copr enable g3tchoo/prismlauncher
+install_dnf prismlauncher
+
+echo "script complete! reboot the system"
